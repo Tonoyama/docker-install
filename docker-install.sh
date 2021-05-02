e1e7db9cfabaaa2ce84ef2af2ff1021d935932de
Tonoyama <yudaitonoyama@gmail.com>
2021-05-02 22:34:56 +0900

    アップデートも追加

--- docker-install.sh
+++ docker-install.sh
@@ -1,3 +1,12 @@
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
